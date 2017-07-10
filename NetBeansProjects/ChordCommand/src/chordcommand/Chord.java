41bd88bca5a3b5175108b78e308ed31f7cbd483b
Charlotte Hirschberger <charlotte.hirschberg@mymail.champlain.edu>
2017-07-09 23:46:04 -0400

    Modified as child of new MusicStructure class

--- NetBeansProjects/ChordCommand/src/chordcommand/Chord.java
+++ NetBeansProjects/ChordCommand/src/chordcommand/Chord.java
@@ -6,35 +6,22 @@
 package chordcommand;
 
 import java.util.ArrayList;
-import java.util.List;
-import javafx.scene.transform.Scale;
 
 /**
  *
  * @author Charlotte
  */
-public class Chord {
-    private MajorKey mKey;
+public class Chord extends MusicStructure {
     private String dbName;
-    private String displayName;
-    private List<Scale> scales;
-    private String strChordNums;
-    private ArrayList<Integer> chordNums;
-    private ArrayList<Integer> chordSuffs;
-    private String strPitches;
+    private int id;
+    private ArrayList<Scale> scales;
     
     public Chord()
     {
-        mKey = null;
+        super();
         dbName = "";
-        displayName = "";
+        id = -1;
         scales = new ArrayList<>();
-        strPitches = "";
-    }
-    
-    public void setMajKey(MajorKey mKey)
-    {
-        this.mKey = mKey;
     }
     
     public void setDBName(String entry)
@@ -42,47 +29,23 @@ public class Chord {
         dbName = entry;
     }
     
-    public void setDisplayName(String origEntry)
+    public void setID(int id)
     {
-        displayName = origEntry;
+        this.id = id;
     }
     
-    public void setChordNums(String nums)
+    public void addScale(Scale scale1)
     {
-        strChordNums = nums;
+        scales.add(scale1);
     }
     
-    public void setChordNumArr(String nums)
+    public int getId()
     {
-        for(int i = 0; i < nums.length(); i++)
-        {
-            char curr = nums.charAt(i);
-            if(curr != ',')
-                chordNums.add((int)curr);
-        }
+        return id;
     }
     
-    public void setChordSuffsArr(String suffs)
+    public ArrayList<Scale> getScaleList()
     {
-        String[] strSuffs = suffs.split(",");
-        for(int i = 0; i < strSuffs.length; i++)
-            chordNums.add(Integer.valueOf(strSuffs[i]));
+        return scales;
     }
-    
-    public void setPitches()
-    {
-        for(Integer i : chordNums)
-        {
-            char pitchName = mKey.getPitch(i);
-            strPitches += String.valueOf(pitchName);
-            int alter = mKey.getSuff(i) + i;
-            strPitches += translateSuff(alter);
-        }
-    }
-    
-    private String translateSuff(int alter)
-    {
-        // Get the map value for key @alter
-    }
-    
 }
