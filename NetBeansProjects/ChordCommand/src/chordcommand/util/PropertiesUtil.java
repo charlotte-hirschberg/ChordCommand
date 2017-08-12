0176ad0a173ae12989371984879610f5e74ef20c
Charlotte Hirschberger <charlotte.hirschberg@mymail.champlain.edu>
2017-08-12 18:28:27 -0400

    Removed ChecksumUtil member to make less brittle.

--- NetBeansProjects/ChordCommand/src/chordcommand/util/PropertiesUtil.java
+++ NetBeansProjects/ChordCommand/src/chordcommand/util/PropertiesUtil.java
@@ -1,21 +1,19 @@
 package chordcommand.util;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.security.NoSuchAlgorithmException;
 import java.util.Properties;
 
 /** 
- * @Course: SDEV 435 ~ Applied Software Practice
- * @Author Name: Charlotte Hirschberger
- * @Assignment ChordCommand
- * @Date: Jun 12, 2017
- * @Description: The PropertiesUtil class contains methods for creating
+ * Description: The PropertiesUtil class contains methods for creating
  * Properties objects from file content and for persisting Properties in files.
+ * <p>Course: SDEV 435 ~ Applied Software Practice</p>
+ * <p>Author Name: Charlotte Hirschberger</p>
+ * <p>Assignment ChordCommand</p>
+ * Created Date: Jun 12, 2017
  */
 public class PropertiesUtil 
 {
@@ -30,24 +28,10 @@ public class PropertiesUtil
      * Load and return properties from file @fileName
      * @param fileName
      * @return Properties--Example: database credentials
-     * @throws FileNotFoundException
      * @throws IOException 
-     * @throws java.security.NoSuchAlgorithmException 
      */
-    public Properties loadParams(String fileName) throws FileNotFoundException, IOException, NoSuchAlgorithmException
+    public Properties loadParams(String fileName) throws IOException
     {
-        boolean isMatch = chkUtil.compareChecksums(fileName);
-        
-        
-        if(isMatch)
-        {
-            System.out.println("Is a match: " + fileName);
-        } 
-        else
-        {
-            System.out.println("Not a match: " + fileName);
-        }
-        
         Properties props = new Properties();
         
         try(InputStream in = new FileInputStream(fileName))
@@ -62,18 +46,15 @@ public class PropertiesUtil
      * Persist Properties in a file.
      * @param props a Properties object
      * @param fileName the name of a file to persist changes in
-     * @throws FileNotFoundException
      * @throws IOException 
-     * @throws java.security.NoSuchAlgorithmException 
      */
     public void saveParamChanges(Properties props, String fileName) 
-            throws FileNotFoundException, IOException, NoSuchAlgorithmException
+            throws IOException
     {
         try(OutputStream out = new FileOutputStream(fileName))
         {
             props.store(out, null);
-        }
-        chkUtil.createCheckFile(fileName);        
+        }  
     }
     
 } // End class PropertiesUtil
