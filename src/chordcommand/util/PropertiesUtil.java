b19fb7aa569e68d603af49903497ce82b3d53c74
Charlotte Hirschberger <chirsch.asm@gmail.com>
2017-08-12 19:55:27 -0400

    Closed iostreams, fixed documentation

--- src/chordcommand/util/PropertiesUtil.java
+++ src/chordcommand/util/PropertiesUtil.java
@@ -1,8 +1,6 @@
 package chordcommand.util;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,12 +8,12 @@ import java.io.OutputStream;
 import java.util.Properties;
 
 /** 
- * @Course: SDEV 435 ~ Applied Software Practice
- * @Author Name: Charlotte Hirschberger
- * @Assignment ChordCommand
- * @Date: Jun 12, 2017
- * @Description: The PropertiesUtil class contains methods for creating
+ * Description: The PropertiesUtil class contains methods for creating
  * Properties objects from file content and for persisting Properties in files.
+ * <p>Course: SDEV 435 ~ Applied Software Practice</p>
+ * <p>Author Name: Charlotte Hirschberger</p>
+ * <p>Assignment ChordCommand</p>
+ * Created Date: Jun 12, 2017
  */
 public class PropertiesUtil 
 {
@@ -23,16 +21,16 @@ public class PropertiesUtil
      * Load and return properties from file @fileName
      * @param fileName
      * @return Properties--Example: database credentials
-     * @throws FileNotFoundException
      * @throws IOException 
      */
     public Properties loadParams(String fileName) throws FileNotFoundException, IOException
     {
         Properties props = new Properties();
         
-        InputStream in = chordcommand.ChordCommand.class.getResourceAsStream(fileName);
-        props.load(in);
-    
+        try(InputStream in = new FileInputStream(fileName))
+        {
+            props.load(in); 
+        }
         
         return props;
     }
@@ -41,14 +39,15 @@ public class PropertiesUtil
      * Persist Properties in a file.
      * @param props a Properties object
      * @param fileName the name of a file to persist changes in
-     * @throws FileNotFoundException
      * @throws IOException 
      */
     public void saveParamChanges(Properties props, String fileName) 
-            throws FileNotFoundException, IOException
+            throws IOException
     {
-        OutputStream out = new FileOutputStream(new File(fileName));
-        props.store(out, null);
+        try(OutputStream out = new FileOutputStream(fileName))
+        {
+            props.store(out, null);
+        }  
     }
     
 } // End class PropertiesUtil
