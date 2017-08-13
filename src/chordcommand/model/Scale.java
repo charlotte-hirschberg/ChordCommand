784f2bcc5abf24b2653ff913f49c58cdfaa30c4d
charlotte.hirschberg <charlotte.hirschberg@mymail.champlain.edu>
2017-08-12 23:42:46 -0400

    Added scale

---
+++ src/chordcommand/model/Scale.java
@@ -0,0 +1,47 @@
+package chordcommand.model;
+
+/** 
+ * Description: An immediate subclass of MusicStructure. Defines the traits
+ * that differentiate a scale from a chord.
+ * <p>Course: SDEV 435 ~ Applied Software Practice</p>
+ * <p>Author Name: Charlotte Hirschberger</p>
+ * <p>Assignment ChordCommand</p>
+ * Created Date: Jun 12, 2017
+ */
+public class Scale extends MusicStructure {
+    private String whForm;
+    
+    /**
+     * Default constructor. Calls the superclass constructor and sets whForm to
+     * empty
+     */
+    public Scale()
+    {
+        super();
+        this.whForm = "";
+    }
+    
+    /**
+     * Constructor that accepts 3 parameters
+     * @param name a string like "Minor"
+     * @param whForm a string of W's, H's, and '-3'
+     * @param mKey a MajorKey object
+     */
+    public Scale(String name, String whForm, MajorKey mKey)
+    {
+        super();
+        this.whForm = whForm;
+        super.setDisplayName(name);
+        super.setMajKey(mKey);
+    }
+    
+    /**
+     * Return the whole-half step representation of this scale. It will be a
+     * mix of spaces, '-3' strings, W's, and H's
+     * @return a string represent the whole-half notation
+     */
+    public String getStrWH()
+    {
+        return whForm;
+    }
+}// End class Scale
